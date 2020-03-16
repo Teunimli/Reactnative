3917b40315282b36abe045016128a959de9b2f68
Thomas Buurstede <am.buurstede@student.avans.nl>
2020-03-16 14:45:27 +0100

    opzet login

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import { Ionicons } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import BottomTabNavigator from './navigation/BottomTabNavigator';
+import LoginScreen from './screens/LoginScreen';
 import useLinking from './navigation/useLinking';
 
 const Stack = createStackNavigator();
@@ -51,7 +51,7 @@ export default function App(props) {
         {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
         <NavigationContainer ref={containerRef} initialState={initialNavigationState}>
           <Stack.Navigator>
-            <Stack.Screen name="Root" component={BottomTabNavigator} />
+            <Stack.Screen name="Login" component={LoginScreen} />
           </Stack.Navigator>
         </NavigationContainer>
       </View>
